90c1a6b7721894b6375b807e2c41fa0fe8abe456
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-08-28 05:20:21 +0530

    Create Reg2.java

---
+++ Reg2.java
@@ -0,0 +1,6 @@
+public class Reg2{
+// Question: Write a regular expression that matches a string that starts with a digit (0-9).
+// Example:
+// Matches: "123abc", "4you"
+// Does Not Match: "abc123", "you4"
+}
