599dd6eceb65fe55b0ea7360a10c79b71c39c8a9
siva-245711 <sivanagendra809@gmail.com>
2024-08-28 20:40:16 +0530

    reg2

--- Reg2.java
+++ Reg2.java
@@ -1,6 +1,21 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Reg2{
 // Question: Write a regular expression that matches a string that starts with a digit (0-9).
 // Example:
 // Matches: "123abc", "4you"
 // Does Not Match: "abc123", "you4"
+
+    public static void main(String[] args) {
+
+        String string_digit_partten = "^\\d.*";
+        Pattern pattern = Pattern.compile(string_digit_partten);
+        Matcher matcher = pattern.matcher("123abc ,4you");
+        if (matcher.matches()) {
+            System.out.println("digit matched");
+        } else {
+            System.out.println("digit not matched");
+        }
+    }
 }
