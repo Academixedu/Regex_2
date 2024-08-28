65d9b93e81d74f505b40065063dcf9e74943a22e
jangamswarupa-595 <swarupajangam9@gmail.com>
2024-08-28 19:05:31 +0530

    Regex

--- Reg3.java
+++
@@ -1,6 +0,0 @@
-public class Reg3{
-// Write a regular expression to match strings that start with any character except "a", "b", or "c".
-// Example:
-// Matches: "def", "xyz", "123abc"
-// Does Not Match: "apple", "bat", "cat"
-}
