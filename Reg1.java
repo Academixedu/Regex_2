12c99658c8531825daba9ce5afaf3ebc56123a5d
syamkuamrnallamilli0 <syamkumarnallamilli0@gmail.com>
2024-08-29 00:05:28 +0530

    done

--- Reg1.java
+++ Reg1.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Reg1{
 // Write a regular expression to validate a 10-digit phone number. The phone number may have optional dashes or spaces separating groups of digits.
 // Valid: 123-456-7890, 123 456 7890, 1234567890
 // Invalid: 12-3456-7890, 123-45-67890
+
+    public static void PhoneNumber(String number){ 
+        String pattern="^(\\d{3}[- ]?\\d{3}[- ]?\\d{4})$";
+        Pattern p = Pattern.compile(pattern);
+        Matcher m = p.matcher(number);
+        if (m.matches()) {
+            System.out.println("Valid Number: " + number);
+        } else {
+            System.out.println("Invalid Number: " + number);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter phone number");
+        String number=sc.nextLine();
+        PhoneNumber(number);
+
+    }
 }
--- Reg2.java
+++ Reg2.java
@@ -1,6 +1,29 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Reg2{
 // Question: Write a regular expression that matches a string that starts with a digit (0-9).
 // Example:
 // Matches: "123abc", "4you"
 // Does Not Match: "abc123", "you4"
+    public static void combination(String Num_Alph){
+        String pattern="^[0-9].*";
+        Pattern p = Pattern.compile(pattern);
+        Matcher m = p.matcher(Num_Alph);
+        if (m.matches()) {
+            System.out.println("Matches: " + Num_Alph);
+        } else {
+            System.out.println("Does not match: " + Num_Alph);
+        }
+        System.out.println();
+
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter your combination");
+        String Num_Alph=sc.nextLine();
+        
+        combination(Num_Alph);
+    }
 }
