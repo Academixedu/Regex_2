f8f3b93ef5041660c094a65aa3278ecfa7a5f0c8
fayazshaik515 <fayazshaik231104@gmail.com>
2024-08-28 19:15:38 +0530

    from fayaz

--- Reg1.java
+++ Reg1.java
@@ -1,5 +1,29 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Reg1{
 // Write a regular expression to validate a 10-digit phone number. The phone number may have optional dashes or spaces separating groups of digits.
 // Valid: 123-456-7890, 123 456 7890, 1234567890
 // Invalid: 12-3456-7890, 123-45-67890
+public static void validatePhoneNumber(String  phonenumber){
+    System.out.println("phone number validtion: ");
+    String pattern = "^(\\d{3}[- ]?\\d{3}[- ]?\\d{4})$";
+    
+    Pattern p = Pattern.compile(pattern);
+    Matcher m = p.matcher(phonenumber);
+
+    if(m.matches()){
+        System.out.println("valid phone number :"+phonenumber );
+    }
+    else{
+        System.out.println("not a valid phone number:"+phonenumber);
+    }
+}
+public static void main(String[] args){
+
+validatePhoneNumber("9014720938");
+validatePhoneNumber("901-472-0938");
+validatePhoneNumber("901 472 0938");
+validatePhoneNumber("12-3456-7890");
+}
 }
--- Reg3.java
+++ Reg3.java
@@ -1,6 +1,34 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Reg3{
 // Write a regular expression to match strings that start with any character except "a", "b", or "c".
 // Example:
 // Matches: "def", "xyz", "123abc"
 // Does Not Match: "apple", "bat", "cat"
+public static void character(String chara){
+    String pattern="^[^abc].*";
+    Pattern p = Pattern.compile(pattern);
+    Matcher m = p.matcher(chara);
+
+     
+    if(m.matches()){
+        System.out.println("matches :"+chara);
+    }
+    else{
+        System.out.println("not match:"+chara);
+    }
+}
+
+
+
+    public static void main(String[] args) {
+        character("def");
+        character("xyz");
+         character("123abc");
+        character("apple");
+       character( "bat");
+       character("cat");
+
+    }
 }
