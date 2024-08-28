3ee2276afcf04a7e454e118f7d753831f17060dd
kavitha-guntru <kavithaguntru60@gmail.com>
2024-08-28 18:58:36 +0530

    kavitha

--- Reg2.java
+++ Reg2.java
@@ -1,6 +1,20 @@
+import java.util.regex.Pattern;
 public class Reg2{
 // Question: Write a regular expression that matches a string that starts with a digit (0-9).
 // Example:
 // Matches: "123abc", "4you"
 // Does Not Match: "abc123", "you4"
+public static void main(String[] args) {
+    String regex = "^\\d";
+    Pattern pattern = Pattern.compile(regex);
+    String[] testStrings = {"123abc", "4you", "abc123", "you4"};
+
+    for (String str : testStrings) {
+        if (pattern.matcher(str).find()) {
+            System.out.println(str + " matches.");
+        } else {
+            System.out.println(str + " doesn't match.");
+        }
+    }
+}
 }
--- Reg3.java
+++ Reg3.java
@@ -1,6 +1,26 @@
+import java.util.regex.Pattern;
 public class Reg3{
 // Write a regular expression to match strings that start with any character except "a", "b", or "c".
 // Example:
 // Matches: "def", "xyz", "123abc"
 // Does Not Match: "apple", "bat", "cat"
+    public static void main(String[] args) {
+        String regex = "^[^abc]";
+        Pattern pattern = Pattern.compile(regex);
+        String[] testStrings = {"def", "xyz", "123abc", "apple", "bat", "cat"};
+
+        for (String str : testStrings) {
+            if (pattern.matcher(str).find()) {
+                System.out.println(str + " matches.");
+            } else {
+                System.out.println(str + " does not match.");
+            }
+        }
+    }
 }
+
+
+    
+
+
+
