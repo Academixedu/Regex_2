4e318e317d6560331bfcbb56b9adaafdcacd358d
sujatha-94 <sujathakarri701@gmail.com>
2024-08-28 18:50:42 +0530

    regex

--- Reg2.java
+++ Reg2.java
@@ -1,6 +1,26 @@
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 public class Reg2{
 // Question: Write a regular expression that matches a string that starts with a digit (0-9).
 // Example:
 // Matches: "123abc", "4you"
 // Does Not Match: "abc123", "you4"
+
+public static void main(String[] args) {
+    String regex = "^\\d.*";
+    String[] strings = {"123abc", "4you", "abc123", "you4"};
+
+    Pattern pattern = Pattern.compile(regex);
+
+    for (String s : strings) {
+        Matcher m = pattern.matcher(s);
+        if (m.matches()) {
+            System.out.println(s + " matches");
+        } else {
+            System.out.println(s + " does not match");
+        }
+    }
+}
+
 }
